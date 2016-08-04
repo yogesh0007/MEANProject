439ff1748e7b33bb4c05d007789e9a95920f086e
Yogesh Prasai <Yogesh Prasai>
2016-08-04 19:47:54 -0400

    adding MongoDB connection

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-router.get('/:userTitle?', function(req, res, next) {
-    res.render('node', {title : req.params.userTitle ? req.params.userTitle : ''});
+router.get('/', function(req, res, next) {
+  var email = '';
+    User.findOne({}, function (err, doc){
+      if(err){
+        return res.send('Error')
+      }
+      var email = doc.email;
+      res.render('node', {email: email});
+    });
 });
 
 router.post('/', function(req, res, next){
-    var title = req.body.title;
-    res.redirect('/' + title);
+    var email = req.body.email;
+    var user = new User({
+      firstName: 'Yogesh',
+      lastName: 'Prasai',
+      password: 'super-secret',
+      email: email,
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
