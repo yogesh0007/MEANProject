f309c57443bba3786d809e0199d402f81260503f
Yogesh Prasai <Yogesh Prasai>
2016-08-03 21:11:01 -0400

    adding new files

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index');
+router.get('/:userTitle?', function(req, res, next) {
+    res.render('node', {title : req.params.userTitle ? req.params.userTitle : ''});
+});
+
+router.post('/', function(req, res, next){
+    var title = req.body.title;
+    res.redirect('/' + title);
 });
 
 module.exports = router;
