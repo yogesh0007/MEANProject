c105156fec0371661e3901e449f701f628101f34
Yogesh Prasai <Yogesh Prasai>
2016-09-29 23:35:22 -0400

    Errors

--- routes/users.js
+++ routes/users.js
@@ -26,32 +26,4 @@ router.post('/', function(req, res, next) {
     });
 });
 
-router.post('/signin', function(req, res, next) {
-   User.findOne({email: req.body.email}, function(err, doc) {
-       if (err) {
-           return res.status(404).json({
-               title: 'An error occurred',
-               error: err
-           });
-       }
-       if (!doc) {
-           return res.status(404).json({
-               title: 'No user found',
-               error: {message: 'User could not be found'}
-           });
-       }
-       if (!passwordHash.verify(req.body.password, doc.password)) {
-           return res.status(404).json({
-               title: 'Could not sign you in',
-               error: {message: 'Invalid password'}
-           });
-       }
-       var token = jwt.sign({user: doc}, 'secret', {expiresIn: 7200});
-       res.status(200).json({
-         message: 'Success',
-         token: token
-       });
-   })
-});
-
 module.exports = router;
